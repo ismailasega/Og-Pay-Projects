cc75ddc3a2513a6f8e283fbf75fe23ac51d7fac3
Ismail Asega <i.asega@ug.isys.mobi>
2020-05-18 16:23:16 +0300

    configurations

--- asp.net_C#/GiveIn.ascx.cs
+++ asp.net_C#/GiveIn.ascx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+#using System.Text;
+#using System.Security.Cryptography;
 
 public partial class UserControls_Donations_GiveIn : System.Web.UI.UserControl
 {
